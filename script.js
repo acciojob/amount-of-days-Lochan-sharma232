8082bd94016ccc416e58bb291ed464634adbf030
Lochan-sharma232 <lochansharma537@gmail.com>
2025-02-16 16:46:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //your JS code here. If required.
-function daysOfAYear(2022){
-	console.log()
-	return 365;
+function daysOfAYear(year){
+	if(year% 4 === 0 && (year % 100 ! == 0 || year % 400 === 0)){
+	return 366;
+	}else{
+		return 365;
+	}
 }
